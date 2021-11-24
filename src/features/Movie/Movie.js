e9ebb60eeb77d98e1746fd441c33dabc9e3d1fb2
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-11-24 18:32:25 +0530

    1 file modified

--- src/features/Movie/Movie.js
+++ src/features/Movie/Movie.js
@@ -5,25 +5,26 @@ export default function Movie() {
   const api_KEY = '9ff11b48756361f4d7fb594dbbb49e87';
   let [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    fetch(
-      'https://api.themoviedb.org/4/list/1?page=1&api_key=9ff11b48756361f4d7fb594dbbb49e87'
-    )
+  useEffect(async () => {
+    await fetch([
+      'https://api.themoviedb.org/4/list/1?page=1&api_key=9ff11b48756361f4d7fb594dbbb49e87',
+    ])
       .then((response) => response.json())
 
       .then((data) => setMovies(data.results));
   });
   const IMG_API = 'https://image.tmdb.org/t/p/w500/';
-
+  console.log(movies);
   return (
     <div>
-      {movies.map((item) => {
-        return (
-          <>
-            <MovieCard item={item} />
-          </>
-        );
-      })}
+      {movies &&
+        movies.map((item) => {
+          return (
+            <>
+              <MovieCard item={item} />
+            </>
+          );
+        })}
     </div>
   );
 }
