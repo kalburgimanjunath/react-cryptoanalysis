857841a2c9e102e520aa9f1c8ac010899f20acd7
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-11-24 18:52:35 +0530

    test

--- src/features/Movie/Movie.js
+++ src/features/Movie/Movie.js
@@ -12,44 +12,44 @@ export default function Movie() {
 
   useEffect(async () => {
     await fetch([
-      // 'https://api.themoviedb.org/4/list/1?page=1&api_key=9ff11b48756361f4d7fb594dbbb49e87',
-      'https://imdb-api.com/en/API/MostPopularTVs/k_ydzj0w61',
+      'https://api.themoviedb.org/4/list/1?page=1&api_key=9ff11b48756361f4d7fb594dbbb49e87',
+      // 'https://imdb-api.com/en/API/MostPopularTVs/k_ydzj0w61',
     ])
       .then((response) => response.json())
 
-      // .then((data) => setMovies(data.results))
-      .then((data) => {
-        console.log(data);
-        setMovies(data.items);
-      })
+      .then((data) => setMovies(data.results))
+      // .then((data) => {
+      //   console.log(data);
+      //   setMovies(data.items);
+      // })
       .catch((error) => console.log(error));
   });
   // const IMG_API = 'https://image.tmdb.org/t/p/w500/';
   console.log(movies);
   const newMovieCard = (item) => {
     return (
-      <Row xs={2} md={4} className="g-4">
-        {Array.from({ length: 4 }).map((_, idx) => (
+      <Row xs={1} md={1} className="g-4">
+        {/* {Array.from({ length: 1 }).map((_, idx) => ( */}
           <Col>
             <Card>
               <Card.Title>{item.title}</Card.Title>
               <Card.Img variant="top" src={item.image} />
             </Card>
           </Col>
-        ))}
+        {/* ))} */}
       </Row>
     );
   };
   return (
     <div>
-      {/* {movies &&
+      {movies &&
         movies.map((item) => {
           return (
             <>
               <MovieCard item={item} />
             </>
           );
-        })} */}
+        })}
       {/* {movies &&
         movies.map((item) => {
           return (
